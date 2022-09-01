d68e27b45a8e4770cd6ecc44bad31471ecb68786
Niyaz Karimov <ml.zyaeen@gmail.com>
2022-09-01 12:34:51 +0300

    Добавлен функционал добавления узлов

--- frontend/visjs-test.js
+++ frontend/visjs-test.js
@@ -16,7 +16,6 @@ class VisJsTest {
         this.network = new Network(this.container, this.data, options);
     }
     addEdge(newNodeId, newEdge){
-        console.log("тут");
         try {
             this.network.body.data.nodes.add([{id: newNodeId, label: "Node " + newNodeId.toString()}])
         } catch (err) {
